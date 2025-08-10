713ff20c1ec21dedaf2f9a0953859d0ccc249b24
Minahil <minahil3955@gmail.com>
2025-08-11 00:43:16 +0500

    Additional Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import "@liveblocks/react-ui/styles.css";
 import "@liveblocks/react-tiptap/styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import DocumentEditor from "./pages/DocumentEditor"
 
 const App: React.FC = () => {
