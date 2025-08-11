76a879bb8bb567b53467c2dc29bda0a6b72a3806
Minahil <minahil3955@gmail.com>
2025-08-11 17:21:54 +0500

    Un necessary Pages folder removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import "@liveblocks/react-ui/styles.css";
 import "@liveblocks/react-tiptap/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DocumentEditor from "./pages/DocumentEditor"
+import DocumentEditor from "./app/pages/DocumentEditor";
 
 const App: React.FC = () => {
   return (
